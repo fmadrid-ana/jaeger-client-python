9dfa35efe0d83b083fc3d67d293e80edb48fc887
Frank Madrid <fmadrid@asmodeena.com>
2019-04-04 20:50:56 -0500

    intial API implementation

--- jaeger_client/scope.py
+++ jaeger_client/scope.py
@@ -1,6 +1,55 @@
 
 
 class Scope():
-    def __init__(self, operation_name, child_of=None, references=None, tags=None, start_time=None):
-        #TODO implement Scope
-        pass
+    """A scope formalizes the activation and deactivation of a :class:`Span`,
+    usually from a CPU standpoint. Many times a :class:`Span` will be extant
+    (in that :meth:`Span.finish()` has not been called) despite being in a
+    non-runnable state from a CPU/scheduler standpoint. For instance, a
+    :class:`Span` representing the client side of an RPC will be unfinished but
+    blocked on IO while the RPC is still outstanding. A scope defines when a
+    given :class:`Span` is scheduled and on the path.
+    :param manager: the :class:`ScopeManager` that created this :class:`Scope`.
+    :type manager: ScopeManager
+    :param span: the :class:`Span` used for this :class:`Scope`.
+    :type span: Span
+    """
+    def __init__(self, manager, span):
+        """Initializes a scope for *span*."""
+        self._manager = manager
+        self._span = span
+
+    @property
+    def span(self):
+        """Returns the :class:`Span` wrapped by this :class:`Scope`.
+        :rtype: Span
+        """
+        return self._span
+
+    @property
+    def manager(self):
+        """Returns the :class:`ScopeManager` that created this :class:`Scope`.
+        :rtype: ScopeManager
+        """
+        return self._manager
+
+    def close(self):
+        """Marks the end of the active period for this :class:`Scope`, updating
+        :attr:`ScopeManager.active` in the process.
+        NOTE: Calling this method more than once on a single :class:`Scope`
+        leads to undefined behavior.
+        """
+        self._manager.active = None
+
+    def __enter__(self):
+        """Allows :class:`Scope` to be used inside a Python Context Manager."""
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Calls :meth:`close()` when the execution is outside the Python
+        Context Manager.
+        If exception has occurred during execution, it is automatically logged
+        and added as a tag to the :class:`Span`.
+        :attr:`~operation.ext.tags.ERROR` will also be set to `True`.
+        """
+        self.span._on_error(exc_type, exc_val, exc_tb)
+        self.close()
--- jaeger_client/scope_manager.py
+++ jaeger_client/scope_manager.py
@@ -1,6 +1,41 @@
+from __future__ import absolute_import
+
+from .span import Span, SpanContext
+from .scope import Scope
 
 
-class ScopeManager():
-    # TODO implement ScopeManager
-    def __init__(self):
-        pass
+class ScopeManager(object):
+    """The :class:`ScopeManager` interface abstracts both the activation of
+    a :class:`Span` and access to an active :class:`Span`/:class:`Scope`.
+    """
+    def __init__(self, span=None, scope=None):
+        # TODO: `tracer` should not be None, but we don't have a reference;
+        # should we move the NOOP SpanContext, Span, Scope to somewhere
+        # else so that they're globally reachable?
+        self._span = span if span else Span(None, SpanContext(), 'None')
+        self._scope = scope if scope else Scope(self, self._span)
+
+    def activate(self, span, finish_on_close):
+        """Makes a :class:`Span` active.
+        :param span: the :class:`Span` that should become active.
+        :param finish_on_close: whether :class:`Span` should be automatically
+            finished when :meth:`Scope.close()` is called.
+        :rtype: Scope
+        :return: a :class:`Scope` to control the end of the active period for
+            *span*. It is a programming error to neglect to call
+            :meth:`Scope.close()` on the returned instance.
+        """
+        return self._scope
+
+    @property
+    def active(self):
+        """Returns the currently active :class:`Scope` which can be used to access the
+        currently active :attr:`Scope.span`.
+        If there is a non-null :class:`Scope`, its wrapped :class:`Span`
+        becomes an implicit parent of any newly-created :class:`Span` at
+        :meth:`Tracer.start_active_span()` time.
+        :rtype: Scope
+        :return: the :class:`Scope` that is active, or ``None`` if not
+            available.
+        """
+        return self._scope
